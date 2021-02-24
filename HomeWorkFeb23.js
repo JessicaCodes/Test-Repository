8a12261ed6bb8bd83aba3a55bce3bd7263625c16
Jessica Salazar <doa.718@gmail.com>
2021-02-23 23:18:55 -0500

    variables hw

---
+++ HomeWorkFeb23.js
@@ -0,0 +1,42 @@
+let myName = "Jessica AKA Jess", myAge = "38", areYouLying = "False",
+        carName = "Volvo", num = 50, num1 = 1, num2 = 5, num3 = 15, num4 = 10,
+         symbol1 = "+", symbol2 = "=", sum = num2 + num4;
+
+
+// 1. Declare a new variable and set it equal to the string of your name
+console.log(myName)
+
+// 2. Declare a new variable and set it equal to a number 
+console.log(myAge)
+
+// 3. Set a variable to a boolean value 
+console.log(areYouLying)
+
+console.log(myAge == areYouLying)
+
+// 4. Create a variable called carName, assign the value Volvo to it.
+console.log(carName)
+
+// 5. Create a variable called num, assign the value 50 to it.
+console.log(num)
+
+// 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+console.log(num1,num2)
+
+console.log(num4,symbol1,num2,symbol2,num3)
+
+console.log('The sum of' +  num4  + 'and' +  num2  + 'is:' +  sum)
+
+// 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+
+console.log(num3)
+
+// 8. Create a set of variables that define an animal (age, height, name, etc...). Must be atleast 6 variables.
+
+let size = "large" , smarts = "intelligent" , color = "gray" , status = "endangered",
+        animal = "elephant"
+
+console.log('I am' + size + smarts + color + 'and' + status + 
+    '. What am I? I am an' + animal)       
+
+

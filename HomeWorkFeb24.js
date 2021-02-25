6ca3cbe3fefe0f442f324ac210d5b7b93f9d9aa2
Jessica Salazar <doa.718@gmail.com>
2021-02-25 00:48:28 -0500

    HomeWorkFeb24

--- HomeWorkFeb24.js
+++ HomeWorkFeb24.js
@@ -5,40 +5,75 @@ let whoAreWe = "We are Software Engineers at Nebula Academy!";
 
 //Log the index of 'software'
 
-console.log(whoAreWe.length)
+//console.log(whoAreWe.length)
 
 //length is 44
 
-let whatEngineer = whoAreWe.indexOf("S",);
+/*const whatEngineer = whoAreWe.indexOf("Software");<--- This didn't work
+    console.log(whatEngineer);*/
 
-console.log(whatEngineer)
+console.log(whoAreWe.indexOf("S"));
+console.log(whoAreWe.indexOf("o"));
+console.log(whoAreWe.indexOf("f"));
+console.log(whoAreWe.indexOf("t"));
+console.log(whoAreWe.indexOf('w', 6));
+console.log(whoAreWe.indexOf('a', 6));
+console.log(whoAreWe.indexOf("r", 6));
+console.log(whoAreWe.indexOf("e", 6));
 
-/*console.log(whoAreWe.indexOf("S"))
-console.log(whoAreWe.indexOf("o"))
-console.log(whoAreWe.indexOf("f"))
-console.log(whoAreWe.indexOf("t"))
-console.log(whoAreWe.indexOf('w'),6)
-console.log(whoAreWe.indexOf("a"),6)
-console.log(whoAreWe.indexOf("r"),6)
-console.log(whoAreWe.indexOf("e"),6)
+//Log the index of 's'
 
-//console.log(whoAreWe.indexOf("s"), "<-- 98: gives me back the index of the character 'l' in the string 'hello'");
+console.log(whoAreWe.indexOf("s"))
+
+//Log the index of 'web'
+
+let spider1 = "web";
+    console.log(spider1.indexOf('w'))
+    console.log(spider1.indexOf('e'))
+    console.log(spider1.indexOf('b'))
+
+//Extract the word 'software' and log it
+
+let word3 = whoAreWe.slice(7,16)
+    console.log(word3)
+
+//Extract the word 'academy' and log it
+
+let word7 = whoAreWe.slice(36,43);
+    console.log(word7)
 
 
-
-
-/*Log the index of 's'
-Log the index of 'web'
-Extract the word 'software' and log it
-Extract the word 'academy' and log it
+/*
 Create a variable and assign the string: "we are learning new string methods"
-Using the string…
+Using the string…*/
 
-Log the length of the string
-Log the last character of the string
-Log the character at index 5
-Uppercase everything in the string
-Lowercase everything in the string
-Create a new variable with your favorite ice cream flavor
-Log the string, 'My favorite ice cream flavor is (your favorite ice cream flavor)'*/
+let newstring = "we are learning new string methods";
+
+//Log the length of the string
+
+console.log(newstring.length);
+
+//Log the last character of the string
+
+console.log(newstring.slice(33));
+
+
+//Log the character at index 5
+
+console.log(newstring[5]);
+
+//Uppercase everything in the string
+
+console.log(newstring.toUpperCase());
+
+//Lowercase everything in the string
+
+console.log(newstring.toLowerCase());
+
+//Create a new variable with your favorite ice cream flavor
+
+let myFaveIceCream = "Chocolate";
+
+//Log the string, 'My favorite ice cream flavor is (your favorite ice cream flavor)'*/
  
+console.log("My favorite icecream is"+ " " + myFaveIceCream);

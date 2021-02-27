cca34e865e21f30fdb79fdddec10bc1a958472f4
Jessica Salazar <doa.718@gmail.com>
2021-02-27 15:46:10 -0500

    fizz buzz exercise - day 1

---
+++ Fizzbuzz_exercise.js
@@ -0,0 +1,41 @@
+/*let num1 = 0
+let counter = 0
+let fizz = 3
+let buzz = 5
+if (num1 / fizz, counter < 100){
+    console.log("Fizz")
+    num1 = num1 + 1
+    counter = counter + 1
+}
+else if (num1 / buzz,  counter < 100){ 
+    console.log("Buzz")
+    num1 = num1 + 1
+    counter = counter + 1
+}
+else {
+    console.log(num1 = num1 + 1)
+}
+    num1 = num1 + 1
+    counter = counter + 1
+    
+*/
+
+let number = 0;
+while (number <= 100){
+
+    if (!number % 3 || 3){
+console.log("fizz", number + 1)
+    number = number + 1
+    }
+    else if (!number / 5 || 5){
+console.log("buzz" , number + 1)
+number = number + 1
+    }
+    else if (!number / 3 || 3);(!number / 5 || 5)
+    {
+    console.log("fizzbuzz", number + 1)
+    number = number + 1
+    }
+    console.log(number + 1 )
+number = number + 1
+}

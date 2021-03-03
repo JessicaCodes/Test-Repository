81abd593c892269a134d8a4295549ea218287417
Jessica Salazar <doa.718@gmail.com>
2021-03-03 09:42:44 -0500

    Folder ReStructuring

--- script.js
+++
@@ -1,5 +0,0 @@
-let primDataTypeVariable = "Primitive Data Types are Number, String, Boolean, Null, Undefined, Symbol";
-
-primDataTypeVariable = "Object is NOT a Primitive Data Type!";
-
-console.log(primDataTypeVariable);

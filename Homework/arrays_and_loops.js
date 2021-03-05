078e871dcbaebe166fd6f18c938c0577f21eb8cc
Jessica Salazar <doa.718@gmail.com>
2021-03-05 00:28:44 -0500

    loops and arrays homework unfinished

---
+++ Homework/arrays_and_loops.js
@@ -0,0 +1,44 @@
+let numArray = [9, 12, 3, 0, 8, 50, 101, 8, 4, 15, 3, 2];
+
+// 1. Using the array above create a function that loops through the array and returns a new array with all the numbers multiplied by 10.
+
+function newArray(array){
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        if (array[i])
+        array[] * 10;
+        newArray.push([]);
+    }
+    return newArray[i]
+}
+
+console.log(newArray(numArray))
+
+
+// 2. Using the array above create a function that returns the sum of all numbers within.
+function findSum(array1){
+    let output = [];
+    for (let i = 0; i < array1.length; i++){
+        
+
+
+    }
+}
+/* 
+3. Create a function that accepts an array and a search value. This function will return true if the search value is an element in the array. Otherwise, it will return false. Don't use any built in array methods to do this. Ie: 
+doesItInclude([23, 24, "kale"], "kale")  -> should return true
+*/
+
+// 4. Create a function that accepts an array. This function will return a new array with the elements in reverse order.
+
+// 5. Create another function that adds the numbers in the array that are NOT divisible by 3 and return the total.
+
+// Use the following array for the questions below (you might need to find examples of "nested loops" using good ol' google):
+
+const nestedArr = ['full', 'stack', ['node', 'react'], [], ['redux'], ['html', 'css'], 'sql'];
+
+// 6. Create a function that logs every word that has the letter 'a' in the word in the array and the nested arrays. Return "Done!" at the end.
+
+// 7. Create a function that logs every word that has a length of 4 in the array and in the nested arrays. Return "Done!" at the end.
+
+// 8. Create a function that logs every word that has the letter 'a' in the word in the array and nested arrays.

88145d48651be8f6b85c4f14750980059341e7aa
DJs Investment Group <doa.718@gmail.com>
2021-04-21 09:34:02 -0400

    canvas tutorial

---
+++ Homework/CanvasTutorial/script.js
@@ -0,0 +1,56 @@
+
+ 
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+
+    // function draw() {
+      
+    //   if (canvas.getContext) {
+        
+
+    //     ctx.fillStyle = 'rgb(200, 0, 0)';
+    //     ctx.fillRect(100, 100, 50, 50);
+
+    //     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    //     ctx.fillRect(120, 120, 50, 50);
+    //   }
+    // }
+
+    
+    // draw()
+
+   const ctx = document.getElementById('canvas').getContext('2d');
+   const ctx2 = document.getElementById('canvas2').getContext('2d');
+    function drawGrid(ctx) {
+       
+        for (let i = 0; i < 6; i++) {
+          for (let j = 0; j < 6; j++) {
+            ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' +
+                             Math.floor(255 - 42.5 * j) + ')';
+            ctx.beginPath();
+            ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
+            ctx.stroke();
+          }
+        }
+      }
+
+      drawGrid(ctx)
+      drawGrid(ctx2)
+
+
+// function draw() {
+//   var ctx = document.getElementById('canvas').getContext('2d');
+//   var img = new Image();
+//   img.onload = function() {
+//     ctx.drawImage(img, 0, 0);
+//     ctx.beginPath();
+//     ctx.moveTo(30, 96);
+//     ctx.lineTo(70, 66);
+//     ctx.lineTo(103, 76);
+//     ctx.lineTo(170, 15);
+//     ctx.stroke();
+//   };
+//   img.src = 'https://mdn.mozillademos.org/files/5395/backdrop.png';
+// }
+
+// draw()
--- Homework/ExtraCreditFeb25.js
+++ Homework/ExtraCreditFeb25.js
@@ -2,19 +2,20 @@
 
 //1. Temperature Converter Lab
 
-let fahr = (78);
+function tempConverter(temp){
+let fahr = temp;
 let fahrToCelc = (fahr - 32) / 1.8;
 let fahrToKelv = (fahr + 459.67) / 1.8;
 console.log(fahr + "° Fahrenheit converts to " + Math.round(fahrToCelc) + "° Celcius and " + Math.round(fahrToKelv) + "° Kelvin");
 
 
-let kelv = (45);
+let kelv = (temp);
 let kelvToCelc = kelv - 273.15;
 let kelvToFahr = kelv * 1.8 - 459.67;
 console.log(kelv + "° Kelvin converts to " + Math.round(kelvToCelc) + "° Celcius and " + Math.round(kelvToFahr) + "° Fahrenheit");
 
 
-let celc = (45);
+let celc = (temp);
 let celcToFahr = (celc * 1.8 + 32);
 let celcToKelv = (celc + 273.15);
 console.log("Did you know that " + celc + "° Celcius converts to " + Math.round(celcToFahr) + 
@@ -22,7 +23,11 @@ console.log("Did you know that " + celc + "° Celcius converts to " + Math.round
 
 /*2. Ceate a variable and assign the string: "we are software engineers at nebula academy"
    then In ONE LINE - extract the word "software" and log it.*/
+}
+
+console.log(tempConverter(102))
+
 
    
-let whoAreWe = "We are Software Engineers at Nebula Academy";
-console.log(whoAreWe.slice(7,15))
+// let whoAreWe = "We are Software Engineers at Nebula Academy";
+// console.log(whoAreWe.slice(7,15))

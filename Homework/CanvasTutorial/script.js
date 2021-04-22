55601a703510d15b89ba854054f16fa602ddce57
DJs Investment Group <doa.718@gmail.com>
2021-04-22 00:02:05 -0400

    Bubble Updates

--- Homework/CanvasTutorial/script.js
+++ Homework/CanvasTutorial/script.js
@@ -28,7 +28,7 @@
             ctx.strokeStyle = 'rgb(0, ' + Math.floor(255 - 42.5 * i) + ', ' +
                              Math.floor(255 - 42.5 * j) + ')';
             ctx.beginPath();
-            ctx.arc(20 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
+            ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
             ctx.stroke();
           }
         }
